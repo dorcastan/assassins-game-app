c40d8b79ec9983745840fda17d81c9dd0d8c3ec0
dorcastan <dorcas.tann@gmail.com>
2020-03-11 18:25:32 +0800

    Update: Seeds
    Include all house comm members

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-player_names = ['Marcus', 'Nawat', 'Phoebe', "Yi Leo"]
+player_names = [
+    'Marcus', 'Nawat', 'Phoebe', "Yi Leo", "Dana", "Isa", "Jasmine", 
+    "Hannah", "Lynette", "Melody", "Nigel", "Sheng Wei", "Harini"
+]
 
 for name in player_names
     Player.create(name: name, level: 1, status: true, points: 0)
